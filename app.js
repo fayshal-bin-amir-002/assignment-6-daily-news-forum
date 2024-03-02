e26606b78e02bf8f61504f0a1f69240fb713dda8
fayshal69 <foyshalbinamir@gmail.com>
2024-03-02 21:03:29 +0600

    finided discuss section design

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 const searchInputBox = document.getElementById('search-input-box'); console.log(searchInputBox);
+const searchHistoryContainer = document.getElementById('search-history-container'); 
+const displayCardContainer = document.getElementById('displayCardContainer'); 
